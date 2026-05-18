efd594ae9a8cf6591325d0d7f729712e500ce652
Hēsperus <hesprs@outlook.com>
2026-05-19 00:41:57 +0800

    chore(lint): fix lint

--- src/render.ts
+++ src/render.ts
@@ -1,7 +1,7 @@
 import { domToSvg } from 'dom-svg-parser';
 import optimizeSvgNumbers from './utils/optimize-svg';
 export default async function renderToString(element: HTMLElement) {
-	const  { svg } = await domToSvg(element);
+	const { svg } = await domToSvg(element);
 	optimizeSvgNumbers(svg, 4);
 	return svg.outerHTML;
 }
